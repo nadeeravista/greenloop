05881d80e37356397d5c2318d5834b132a702fe3
Nadeera Pathirana <nadeerapathirana@Nadeeras-MacBook-Pro.local>
2025-11-29 18:57:18 +1300

    chore: Convert user controller to multi function route controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UserService;
+
 class UserController extends Controller
 {
     /**
@@ -25,10 +27,37 @@ class UserController extends Controller
      *     )
      * )
      */
-    public function __invoke()
+    public function index()
     {
         return response()->json([
             'message' => 'Authenticated'
+        ], 200);
+    }
+
+
+    /**
+     * @OA\Get(
+     *     path="auth/generate-qr-code",
+     *     summary="Generate QR Code endpoint",
+     *     description="Generates a QR Code",
+     *     tags={"QR Code"},
+     *     security={{"sanctum": {}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="QR Code generated")
+     *         )
+     *     )
+     * )
+     */
+    public function generateQRCode(UserService $userService)
+    {
+        // $qrCode = $userService->generateQRCode();
+
+        return response()->json([
+            'message' => 'QR Code generated',
+            'qr_code' => 'https://www.google.com'
         ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,11 @@ use App\Http\Controllers\HealthController;
 use App\Http\Controllers\UserController;
 
 Route::get('/health', HealthController::class);
-Route::get('/auth', UserController::class)->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Standard REST resource routes (add methods to controller as needed)
+    Route::apiResource('auth', UserController::class)->only(['index']);
+
+    // Custom routes - add more custom routes here as needed
+    Route::get('/auth/generate-qr-code', [UserController::class, 'generateQRCode']);
+});
