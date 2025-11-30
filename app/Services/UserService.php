4b8d1fd4583bfe5265c2ce95f3dd59a3e97d2b9e
Nadeera Pathirana <nadeerapathirana@Nadeeras-MacBook-Pro.local>
2025-11-30 13:14:14 +1300

    chore: get User modlel using app container resolutin so that can be mocked easiliy

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,12 +3,13 @@
 namespace App\Services;
 
 use App\Models\User;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class UserService
 {
     public function generateToken()
     {
-        $user = User::first();
+        $user = app(User::class)::first();
 
         if (!$user) {
             throw new \Exception('User not found');
@@ -19,5 +20,17 @@ class UserService
         return response()->json([
             'token' => $token
         ], 200);
+    }
+
+    /**
+     * @param User $user
+     * @return string QR code data
+     */
+    public function generateQRCode(string $email): string
+    {
+        $data = $email;
+
+        return QrCode::size(200)
+            ->generate($data);
     }
 }
