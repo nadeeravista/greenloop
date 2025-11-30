f73c26afad52cbd00e851531dc10627f646bfe33
Nadeera Pathirana <nadeerapathirana@Nadeeras-MacBook-Pro.local>
2025-11-30 13:16:45 +1300

    chore: adding test - mocking as much ass possible instead using DB

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Mockery;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserControllerTest extends TestCase
+{
+
+    public function test_auth_test_endpoint()
+    {
+
+        $request = Mockery::mock(Request::class);
+        $request->shouldReceive('user')->andReturn(new User([
+            'name' => 'Test User',
+            'id' => 1,
+            'email' => 'test@example.com'
+        ]));
+
+        // Bind the mock to Laravel's container
+        $this->app->instance(Request::class, $request);
+
+        $response = $this->withoutMiddleware()->get('/api/auth');
+        $response->assertStatus(200);
+        $response->assertJson([
+            'message' => 'Authenticated'
+        ]);
+    }
+
+    public function test_generate_qr_code_endpoint_returns_successful_response()
+    {
+
+        $request = Mockery::mock(Request::class);
+        $request->shouldReceive('user')->andReturn(new User([
+            'name' => 'Test User',
+            'id' => 1,
+            'email' => 'test@example.com'
+        ]));
+
+        // Bind the mock to Laravel's container
+        $this->app->instance(Request::class, $request);
+
+        $response = $this->withoutMiddleware()->get('/api/auth/generate-qr-code');
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'message' => 'QR Code generated'
+        ]);
+        $response->assertJsonStructure([
+            'content'
+        ]);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+}
---
+++ tests/Unit/Services/UserServiceTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use App\Services\UserService;
+use Mockery;
+use App\Models\User;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class UserServiceTest extends TestCase
+{
+    public function test_generate_qr_code()
+    {
+        $userService = new UserService();
+
+        $qrCode = $userService->generateQRCode('test@example.com');
+
+        $this->assertNotNull($qrCode);
+        $this->assertStringStartsWith('<?xml version="1.0"', $qrCode);
+    }
+
+    public function test_generate_token()
+    {
+
+        $mockPlainTextToken = Mockery::mock();
+        $mockPlainTextToken->plainTextToken = 'mocked-token-12345';
+
+        $mockFirstUser = Mockery::mock();
+        $mockFirstUser->shouldReceive('createToken')
+            ->with('api-token')
+            ->once()
+            ->andReturn($mockPlainTextToken);
+
+
+        $mockUser = Mockery::mock(User::class);
+        $mockUser->shouldReceive('first')->andReturn($mockFirstUser);
+
+        $this->app->instance(User::class, $mockUser);
+
+        $userService = new UserService();
+        $token = $userService->generateToken();
+
+        $this->assertEquals('mocked-token-12345', $token);
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+}
